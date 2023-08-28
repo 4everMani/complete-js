9adeb853fd8fe5bdfd8b2b9a412498bc5172b204
Manish Jaiswal <manishjaiswal@Manishs-Mac-mini.local>
2023-08-28 06:43:33 +0530

    values and variable added

---
+++ 01_value.js
@@ -0,0 +1,30 @@
+// Premitive Datatype/Values
+    // Number values
+    7
+    3.6
+
+    // String values
+    'manish'
+    "manish"
+
+    // Boolean values
+    true
+    false
+
+    // Empty values
+    null
+    undefined
+
+// Non-primitive / Adavanced Datatype
+
+    // Array
+    [1,2,3,4,5,6,7,"manish",true] // can store different datatypes
+
+    // Object
+    {
+        fname = 'manish',
+        lname = 'jaiswal'
+    }
+
+
+// Primitive datatype will have only one value, non-primitive datatype may have more than one values
---
+++ 02_variable.js
@@ -0,0 +1,17 @@
+// variable is a placeholder to hold value.
+
+// there are three ways to declare a variable
+// 1) var
+// 2) let => variable whose value will change in future like account balance
+// 3) const => variable whose value is not going to change in future like bank account number
+
+var name = 'manish';
+let age = 24;
+const mobileNumber = 6653345498;
+
+console.log("Name:",name);
+console.log("Age:",age);
+console.log("MobileNumber:",mobileNumber);
+
+// template literal
+console.log(`My name is ${name} and age is ${age}, you can contact me on ${mobileNumber}`);
